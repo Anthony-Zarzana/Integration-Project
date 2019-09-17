97b48b365630bf5706ef84fad224d346d868b699
Anthony <aczarzana8259@eagle.fgcu.edu>
2019-09-17 13:46:51 -0400

    this is my first commit

---
+++ Main.py
@@ -0,0 +1,7 @@
+#Anthony Zarzana
+#Google Project
+print("Welcome to my Google Project")
+value1 = input("Give me a number")
+value2 = input("Give me another number")
+total = value1+value2
+print("Your total is ",total)
